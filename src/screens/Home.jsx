6b8b1447c2b09195bfe0f32eb0dfa6fe81573c12
Maiten Cullen <maitencullen@gmail.com>
2023-01-08 13:50:56 -0300

    inicialice vite y sass

---
+++ src/screens/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+export const Home = () => {
+  return (
+    <div>
+        <ul className='navBarUl'>
+            <li>Proyectos</li>
+            <li>Servicios</li>
+            <li>Habilidades Técnicas</li>
+            <li>Contacto</li>
+        </ul>
+    </div>
+  )
+}
