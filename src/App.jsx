47ed7d8686892ddea8358b8677c945a7afa58ea8
Maiten Cullen <maitencullen@gmail.com>
2023-01-30 17:46:59 -0300

    dark and light theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import {Contact} from './screens/Contact'
 import { Home } from './screens/Home'
@@ -6,10 +6,20 @@ import { Skills } from './screens/Skills'
 import { Projects } from './screens/Projects';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+const [ theme, setTheme] = useState('light')
+const toggleTheme = () => {
+  if (theme === 'light'){
+    setTheme('dark');
+  } else {
+    setTheme('light');
+  }
+};
+useEffect(() => {
+  document.body.className = theme;
+}, [theme]);
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Dame Click</button>
          <Home/>
          <Skills/>
          <Contact/>
